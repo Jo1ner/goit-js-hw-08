be0b327ee76d1a1128f70b00a17ce33834eda31c
Jo1ner <semdimas@gmail.com>
2023-08-30 15:32:22 +0300

    fix task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,51 +5,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-
+// listEl.style.listStyle = 'none';
 const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-let instance;
+
 function createGalleryMarkup(galleryItems) {
     const markup = galleryItems.map(({ preview, original, description }) => {
-        return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`;
+        return `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`;
     }).join("");
     return markup;
 }
 
-function openModal(event) {
-  event.preventDefault();
-  
-  if (event.target.nodeName !== 'IMG') return;
-  
-  const imageSrc = event.target.getAttribute('src');
-  const imageAlt = event.target.getAttribute('alt');
-  
-  const instance = basicLightbox.create(
-    `<div class="modal">
-    <img src="${imageSrc}" alt="${imageAlt}">
-    </div>`);
-  
-    instance.show();
-    document.addEventListener('keydown', closeModalPressEscape);
-};
-
-function closeModalPressEscape(event) {
-  if (event.code === 'Escape') {
-
-    instance.close();
-    document.removeEventListener('keydown', closeModalPressEscape);
-  }
-};
-
-galleryContainer.addEventListener('click', openModal);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+});
