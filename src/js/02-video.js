4ffc26c1b62aad4386b8c209fab85672feee03cb
Jo1ner <semdimas@gmail.com>
2023-08-31 14:23:09 +0300

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(() => {
+  player.getCurrentTime().then((time) => {
+        localStorage.setItem('videoplayer-current-time', time);
+  });
+}, 1000));
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
+console.log(onPlay)
+
+
+
+  
+
 
